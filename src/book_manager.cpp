c9d8b38b075ac3116196f67c60196e723d07bd77
lmc123456 <lmc123456.fy@qq.com>
2025-12-28 01:53:24 +0800

    修复了modify ISBN时登录栈同步问题

--- src/book_manager.cpp
+++ src/book_manager.cpp
@@ -73,6 +73,7 @@ void BookManager::modify(Book new_inf, AccountManager &account_manager,
       memcpy(keyw.ISBN, new_inf.ISBN, 21);
       keywordbook_.insert(keyw);
     }
+
   }
   if (flag == 2) {
     bo.deleteinformation(tmp);
@@ -98,6 +99,14 @@ void BookManager::modify(Book new_inf, AccountManager &account_manager,
     bo.deleteinformation(tmp);
     bo.insert(new_inf);
     std::vector<keywordchar> key = tokekeyword(tmp.keyword);
+    char cm[61];
+    memset(cm,0,61);
+    for (int i=0;i <= key.size()-1;i++) {
+      if (memcmp(cm,key[i].ans,61) == 0) {
+        std::cout << "Invalid\n";
+        return;
+      }
+    }
     for (size_t i = 0; i < key.size(); i++) {
       keywordbook keyw;
       memcpy(keyw.keyword, key[i].ans, 61);
